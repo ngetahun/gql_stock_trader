f199112aad003f325d8bc5fff4ca40d6a10f430b
Natnael Getahun <connect@ngetahun.me>
2018-12-19 15:15:07 +0100

    update superclass for query

--- server/gql/types/query.rb
+++ server/gql/types/query.rb
@@ -1,6 +1,7 @@
 require 'graphql'
+require_relative 'base_object'
 module Types
-    class Query < GraphQL::Schema::Object
+    class Query < Types::BaseObject
         graphql_name 'RootQuery'
         field :ping, String, description: 'Ping query', null: false
         def ping
