5a0d0e0d2097c7473a2a7e81669e439b1d2aebaa
Natnael Getahun <connect@ngetahun.me>
2020-02-01 17:58:27 +0100

    Add symbol price ticker fetch query

--- server/gql/types/query.rb
+++ server/gql/types/query.rb
@@ -1,5 +1,9 @@
 require 'graphql'
 require_relative 'base_object'
+require_relative 'inputs/symbol_data_input'
+require_relative 'symbol_data'
+require_relative '../lib/fetch_symbol'
+
 module Types
     class Query < Types::BaseObject
         graphql_name 'RootQuery'
@@ -7,6 +11,18 @@ module Types
         def ping
           'pong'
         end
-        field :ping, String, description: '', null: false 
+
+				field :data_for_symbol, [::Types::SymbolData::Symbol] , 'Query to find the latest data for symbol' , null:false do
+					argument :symbol_query, ::Types::Inputs::SymbolDataInput, required: true
+				end
+
+				def data_for_symbol(symbol_query:)
+					Lib::FetchSymbol.ticker_price symbol_query.symbol_data
+				end
+
+				field :symbol_list, [::Types::SymbolData::SymbolItem], 'Fetch Symbol list data', null: false
+				def symbol_list
+					Lib::FetchSymbol.symbols
+				end
     end
 end
