1cdfbb0da0ff180d45f4f0df5980f2700487e6cd
ngetahun <natnael.getahun@suse.com>
2022-08-12 18:31:09 +0200

    Use nuxt framework

---
+++ client/nuxt.config.js
@@ -0,0 +1,58 @@
+import { sortRoutes, extendRoutes } from '@nuxt/utils'
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'client',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: '/dashboard',
+        components: {
+          default: resolve(__dirname, 'pages/dashboard.vue'), // or routes[index].component
+        },
+      })
+    }
+  },
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ client/pages/dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'DashboardPage'
+}
+</script>
